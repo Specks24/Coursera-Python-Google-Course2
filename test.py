c28ec4fd0e71d6f3c59bbae9d47e54a3d72ce7c2
placeholder <kdtran24@gmail.com>
2020-04-12 03:16:17 -0400

    Changed file name to "health_check.py". Added a py script to check PC health (disk usage and cpu usage).

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-#this is being written to test the pycharm integration with GitHub VCS
-
-#this file is being modified in VS IDE - Test for repo config
-
-
-print("hello world")
