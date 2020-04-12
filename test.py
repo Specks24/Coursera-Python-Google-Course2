73294da96727e47dffd4555d48dbb3470b5eb921
placeholder <kdtran24@gmail.com>
2020-04-11 22:04:23 -0400

    Adjusting the file contents to test the VS IDE

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 #this is being written to test the pycharm integration with GitHub VCS
 
+#this file is being modified in VS IDE - Test for repo config
+
 
 print("hello world")
